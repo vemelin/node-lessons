583f40bc8b9bdeca0b7957e8173910e6db31c10d
vemelin <vemelin@gmail.com>
2025-04-20 13:09:40 -0500

    added chalk color set for the second task to hilighte user names

--- chat.js
+++ chat.js
@@ -1,11 +1,32 @@
 import { EventEmitter } from 'node:events';
+import chalk from 'chalk';
+const color = [
+  `red`,
+  `green`,
+  `yellow`,
+  `blue`,
+  `magenta`,
+  `cyan`,
+  `white`,
+  `blackBright`,
+  `redBright`,
+  `greenBright`,
+  `yellowBright`,
+  `blueBright`,
+  `magentaBright`,
+  `cyanBright`,
+  `whiteBright`,
+];
 
 const chat = new EventEmitter();
 
 // Recive Message function
 const recieveMessage = () => {
   chat.on('message', ({username, message}) => {
-    console.log(`${username}: ${message}`);
+    // console.log(`${username}: ${message}`);
+    console.log(`
+      ${chalk[color[Math.floor(Math.random() * color.length)]](username)}: ${message}
+    `);
   });
 };
 
